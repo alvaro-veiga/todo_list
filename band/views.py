1172170419a55e32116c2bd089e4505fa591aeae
Alvaro Veiga <alvarojoao17@gmail.com>
2024-05-19 17:13:12 -0300

    save work

--- band/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from .models import Band
-
-def band_listing(request):
-    """A view of all bands."""
-    bands = Band.objects.all()
-    return render(request, "bands/band_listing.html", {"bands": bands})
